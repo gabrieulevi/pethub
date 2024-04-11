ca9bfb0b52d96ec1512ce3f5be2cd22a97822bc3
gabrieulevi <gabriel@newnet.com.br>
2024-04-11 18:01:01 -0300

    ajustando

--- app/src/app/components/petcard/index.tsx
+++ app/src/app/components/petcard/index.tsx
@@ -1,37 +1,50 @@
-import "./_Petcard.scss"
-import axios from "axios"
-import { 
-    Card,
-    CardTitle,
-    CardDescription 
+import "./_Petcard.scss";
+import axios from "axios";
+import { Card, CardTitle, CardDescription } from "@/components/ui/card";
+import PetAvatar from "./petAvatar";
+import { Button } from "@/components/ui/button";
+import stopwatch from "@/app/components/petcard/assets/stopwatch-svgrepo-com.svg"
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
-} from "@/components/ui/card"
-import { useEffect, useState } from "react"
 
-
-export default function PetCard(){
-    const [pet, setPet] = useState([])
-    const getPet = async () => {
-        await axios.get("https://dog.ceo/api/breeds/image/random").then(response => {
-            setPet(response.data)
-        })
-    }
+export default function PetCard() {
+    const [buttonStatus, setButtonStatus] = useState()
+    const [buttonStyle, setButtonStyle] = useState()
     useEffect(() => {
-        getPet()
+        setButtonStatus(true)
     }, [])
-    return(
-        <Card className="flex flex-row space-x-1.5 start w-full p-10">
-            <div className="image-container">
-            <img src={pet.message} alt="pet image"/>
-            </div>
-            <div className="container">
-            <CardTitle>
-                Fulano de tal
-            </CardTitle>
-            <CardDescription>
-                Agendado para banho e tosa
-            </CardDescription>
-            </div>
-        </Card>
-    )
+    
+    useEffect(() => {
+        if (buttonStatus){
+            setButtonStyle({
+                backgroundColor: "red"
+            })
+        } else {
+            console.log(buttonStatus)
+            setButtonStyle({
+                backgroundColor: "black"
+        
+            })
+        }
+    }, [buttonStatus])
+  return (
+    <Card className="card flex flex-row space-x-2 justify-between w-full h-40 max-w-sm min-h-40  rounded-2xl p-2">
+      <PetAvatar />
+      <div className="card-content-container flex-column w-full h-full justify-between ">
+        <div className="card-text-container">
+        <CardTitle className="text-lg">Frida</CardTitle>
+        <CardDescription>Cliente: Dona Flor da Silva</CardDescription>
+        </div>
+        <div className="button-container flex h-min bg-white flex-row justify-end">
+          <Button className="button" style={buttonStyle} onClick={
+            useEffect(() =>{
+                setButtonStatus(false)
+            },[buttonStyle, buttonStatus])
+            }
+           ><span><Image  alt="stopwatch" src={stopwatch}/></span>Iniciar atendimento</Button>
+        </div>
+      </div>
+    </Card>
+  );
 }
---
+++ app/src/app/components/petcard/petAvatar/index.tsx
@@ -0,0 +1,19 @@
+import "./_PetAvatar.scss"
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+export default function PetAvatar(){
+    const [pet, setPet] = useState([])
+    const getPet = async () => {
+        await axios.get("https://dog.ceo/api/breeds/image/random").then(response => {
+            setPet({backgroundImage: "url('" + response.data.message + "')"})
+        })
+    }
+    useEffect(() => {
+        getPet()
+    }, [])
+    return(
+        <div className="image-container" style={pet}>
+        </div>
+    )
+}
